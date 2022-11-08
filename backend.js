f95846338263d4badd60b6acf50c027045f51b68
Hongliang Wei <hongliang.wei@dolphindb.com>
2022-11-08 22:14:57 +0800

    working demo

---
+++ backend.js
@@ -0,0 +1,40 @@
+const cors           = require('cors'),
+      fs             = require('fs-extra'),
+      formidable     = require('formidable');
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+      
+      
+app.get('/', function(req,res) {
+  res.sendfile('index.html');
+});
+      
+app.post('/fileupload',  (req, res) => {
+
+     const path = './client/public/upload/';
+
+     var form = new formidable.IncomingForm();
+     form.uploadDir = path;
+     form.encoding = 'binary';
+
+     form.parse(req, function(err, fields, files) {
+          if (err) {
+               console.log(err);
+               res.send('upload failed')
+          } else {
+               var oldpath = files.files.path;
+               var newpath = path + files.files.name;
+               fs.rename(oldpath, newpath, function (err) {
+                    if (err) throw err;
+                    res.send('complete').end();
+               });
+          }
+     });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
