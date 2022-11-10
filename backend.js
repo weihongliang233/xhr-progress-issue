7294d42fdf28edb3399d9d901a52f5f44a2e95b4
weihongliang233 <weijohn158@gmail.com>
2022-11-11 00:14:04 +0800

    formatting

--- backend.js
+++ backend.js
@@ -1,40 +1,38 @@
-const cors           = require('cors'),
-      fs             = require('fs-extra'),
-      formidable     = require('formidable');
-
+const cors = require('cors');
+const fs = require('fs-extra');
+const formidable = require('formidable');
 const express = require('express')
 const app = express()
 const port = 3000
 
-      
-      
-app.get('/', function(req,res) {
-  res.sendfile('index.html');
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
 });
-      
-app.post('/fileupload',  (req, res) => {
 
-     const path = './client/public/upload/';
+app.post('/fileupload', (req, res) => {
 
-     var form = new formidable.IncomingForm();
-     form.uploadDir = path;
-     form.encoding = 'binary';
+    const path = './client/public/upload/';
 
-     form.parse(req, function(err, fields, files) {
-          if (err) {
-               console.log(err);
-               res.send('upload failed')
-          } else {
-               var oldpath = files.files.path;
-               var newpath = path + files.files.name;
-               fs.rename(oldpath, newpath, function (err) {
-                    if (err) throw err;
-                    res.send('complete').end();
-               });
-          }
-     });
+    var form = new formidable.IncomingForm();
+    form.uploadDir = path;
+    form.encoding = 'binary';
+
+    form.parse(req, function(err, fields, files) {
+        if (err) {
+            console.log(err);
+            res.send('upload failed')
+        } else {
+            var oldpath = files.files.path;
+            var newpath = path + files.files.name;
+            fs.rename(oldpath, newpath, function(err) {
+                if (err) throw err;
+                res.send('complete').end();
+            });
+        }
+    });
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
+    console.log('Open http://localhost:3000/')
 })
